f81e906296637ad8b2094c427d0caeb99e80bc66
JubbaMan <jubbathegreat@gmail.com>
2026-06-14 23:45:43 +0600

    some fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,37 +2,36 @@ import React from 'react'
 import './Navbar.css'
 
 const Navbar = () => {
+  // Add smooth scrolling function
+  const handleNavigation = (e, sectionId) => {
+    e.preventDefault();
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ 
+        behavior: 'smooth',
+        block: 'start'
+      });
+    }
+  };
+
   return (
     <header className="navbar-wrapper">
-
       <nav className="navbar">
-
-       
-
         <ul className="nav-links">
-
           <li>
-            <a href="#home">Home</a>
-          </li>
-
-          <li>
-            <a href="#memories">Wishes</a>
+            <a href="#hero" onClick={(e) => handleNavigation(e, 'hero')}>Home</a>
           </li>
           <li>
-            <a href="#gifts">Letter</a>
+            <a href="#wishes" onClick={(e) => handleNavigation(e, 'wishes')}>Wishes</a>
           </li>
-
           <li>
-            <a href="#game">Game</a>
+            <a href="#gifts" onClick={(e) => handleNavigation(e, 'gifts')}>Letter</a>
           </li>
-
-
+          <li>
+            <a href="#game" onClick={(e) => handleNavigation(e, 'game')}>Game</a>
+          </li>
         </ul>
-
-        
-
       </nav>
-
     </header>
   )
 }
